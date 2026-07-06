8d23ead9bf1a00bc572166e7c8392e923358bcb4
Eduard Ritt <eduard.ritt@ritt-itservice.de>
2026-07-06 19:03:07 +0200

    delete project for redesign

--- app/src/main/java/de/rittitservice/frequenzia/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package de.rittitservice.frequenzia.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val SignalRot = Color(0xFFE1261C)
-val PaperWhite = Color(0xFFF7F5F0)
-val InkBlack = Color(0xFF1A1A1A)
-val SurfaceGrey = Color(0xFFEAE7E0)
