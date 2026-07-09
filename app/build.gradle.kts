da80674e18e142a4e895f2596c7c192bc88607f2
Eduard Ritt <eduard.ritt@ritt-itservice.de>
2026-07-09 17:17:58 +0200

    Release 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "de.rittitservice.frequenzia"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.5.0"
+        versionCode = 10
+        versionName = "1.5.1"
     }
 
     signingConfigs {
